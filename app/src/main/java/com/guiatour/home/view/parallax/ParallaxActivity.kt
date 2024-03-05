8c1700987e953aabfcfa5185adaca3472b342b02
Apolo  Rossi <apolorossi@Apolos-MBP.local>
2024-03-04 23:56:55 -0300

    Created sample with parallax animation.

--- app/src/main/java/com/guiatour/home/view/parallax/ParallaxActivity.kt
+++ app/src/main/java/com/guiatour/home/view/parallax/ParallaxActivity.kt
@@ -2,53 +2,133 @@ package com.guiatour.home.view.parallax
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Build
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.guiatour.R
 import com.guiatour.ui.theme.GuiaTourTheme
+import com.guiatour.ui.theme.Purple700
+import kotlin.math.min
 
-class ParallaxActivity : AppCompatActivity() {
+class ParallaxActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            GuiaTourTheme(false) {
-                ParallaxComponent()
+            GuiaTourTheme {
+                Surface {
+                    ParallaxComponent()
+                }
             }
+        }
     }
-}}
+}
 
 @Preview
 @Composable
 fun ParallaxComponent() {
     val scrollState = rememberScrollState()
-    val context = LocalContext.current
 
-    Surface(modifier = Modifier.fillMaxWidth()) {
-        LazyColumn(modifier = Modifier) {
+    Box {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+        ) {
 
-            // LazyRow still able to be use, since it has different orientation with parent LazyColumn
-            item {
-                LazyRow(modifier = Modifier.fillMaxWidth()) {
-                    items(listOf("A", "B", "A", "B", "A", "B", "A", "B", "A", "B", "A", "B").size) { story ->
-                        Text(text = "Story $story")
-                    }
-                }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(500.dp)
+                    .graphicsLayer {
+                        alpha = 1f - (scrollState.value.toFloat() / scrollState.maxValue)
+                        translationY = 0.5f * scrollState.value
+                    },
+                contentAlignment = Alignment.Center
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.av_paulista),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
             }
 
-            items(listOf(1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6).size) { post ->
-                Text(text = "Post $post", modifier = Modifier.height(150.dp))
-            }
+            Text(
+                text = stringResource(id = R.string.app_name),
+                modifier = Modifier.padding(8.dp),
+                style = MaterialTheme.typography.h6
+            )
+
+            Text(
+                text = stringResource(id = R.string.long_text_for_tests),
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+                style = MaterialTheme.typography.body1
+            )
         }
+
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(55.dp)
+            .graphicsLayer {
+                alpha = min(1f, scrollState.value.toFloat() / scrollState.maxValue)
+            }
+            .background(color = Purple700), contentAlignment = Alignment.CenterStart
+        ) {
+            Text(
+                text = "Guia Tour Aplicativo",
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.padding(horizontal = 12.dp),
+                color = Color.White
+            )
+
+        }
+
+
     }
 }
